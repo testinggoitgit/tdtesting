90ca894c965b6cedde48d2d19f184942563c0f10
NotEnoughMax <githubtestinggoit@gmail.com>
2025-05-03 20:17:22 +0300

    Fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
